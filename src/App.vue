50b885c595ee398421f5f851bcee82889ec24c26
wellfrog <wellfrog16@gmail.com>
2017-09-27 19:45:59 +0800

    工作更新

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <main-nav></main-nav>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
+import MainNav from './components/MainNav'
+import './assets/lib/bootstrap/less-3.3.7/bootstrap.less'
+
 export default {
-  name: 'app'
+    name: 'app',
+    components : { MainNav }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+
+* { box-sizing:border-box; }
+img { border:0;}
+a { text-decoration:none;}
+
+html, body {
+    width: 100%;
+    height: 100%;
+
+    padding: 0;
+    margin: 0;
+
+    font-family: 'Miscrosoft Yahei', '微软雅黑';
+    font-size:14px;
+
+    color:#fff;
+    background-color:#000;
+    overflow:hidden;
 }
 </style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="main-nav">
+        <div class="nav1"></div>
+        <div class="nav2"></div>
+        <div class="nav-toggle"></div>
+        <div class="nav"></div>
+        <div class="language"><span>中文</span><span>EN</span></div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name : 'main-nav'
+}
+</script>
+
+<style lang="less">
+
+@import '../assets/style/common.less';
+
+@padding-height : 40px;
+@padding-width: 20px;
+
+.nav1 {
+    width: 16px;
+    height: 14px;
+    background-image: url('../assets/img/icon/triangle-border.png');
+    position: fixed;
+    top: @padding-height;
+    left: @padding-width;
+    cursor: pointer;
+
+    &:hover {
+        background-position-x: -16px;
+    }
+}
+
+.nav2 {
+    width: 15px;
+    height: 14px;
+    background-image: url('../assets/img/icon/triangle-grey.png');
+    position: fixed;
+    bottom: @padding-height;
+    left: @padding-width;
+    cursor: pointer;
+
+    &:hover {
+        background-position-x: -15px;
+    }
+}
+
+.language {
+    position: fixed;
+    bottom: @padding-height;
+    right: @padding-width;
+
+    span {
+        margin-left: 10px;
+        cursor: pointer;
+
+        &:hover {
+            color: @main-color;
+        }
+    }
+}
+</style>
+
+
